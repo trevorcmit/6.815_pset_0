0a371ba53916e20225e19cd774efecc49e81eb36
Trevor <trevorsc@mit.edu>
2021-10-01 11:35:07 -0400

    first commit

---
+++ asst/a0_main.cpp
@@ -0,0 +1,19 @@
+#include "a0.h"
+
+using namespace std;
+
+// This is a way for you to test your functions.
+// We will only grade the contents of a0.cpp and Image.cpp
+int main() {
+  // --------- HANDOUT  PS00 ------------------------------
+  cout << "Congratulations, you have compiled the starter code!" << endl;
+
+  // Example Test
+  // Read an Image
+  // Image im("./Input/Cambridge1.png");
+
+  // Write an Image
+  // cout << "Image size: " << im.width() << ", "
+  //      << im.height() << ", " << im.channels() << endl;
+  // im.write("./Output/SameAsInput.png");
+}
